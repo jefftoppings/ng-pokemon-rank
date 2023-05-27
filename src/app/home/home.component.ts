e816bb223d2b186f7a715bc17a42ecef75eab462
Jeff Toppings <jeffrey.toppings@outlook.com>
2023-05-27 11:48:28 -0600

    feat: add some custom theming colours

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MatToolbarModule],
+})
+export class HomeComponent {}
