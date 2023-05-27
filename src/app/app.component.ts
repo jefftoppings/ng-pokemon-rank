e0692c65b09145839cb2816746eb20db3e97fd28
Jeff Toppings <jeffrey.toppings@outlook.com>
2023-05-27 12:45:51 -0600

    feat: start wiring up autocomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,28 +10,28 @@ export class AppComponent {
   title = 'ng-pokemon-rank';
 
   constructor() {
-    const pokedexEntry = getByName({ name: 'Ninetales' });
-    const { rank, occurence } = calculateRank({
-      pokedexEntry,
-      // refs from pokémon being compared (1-15)
-      refAttackStat: 0,
-      refDefenseStat: 15,
-      refHealthStat: 15,
-      // max league CP
-      maxCP: 1500,
-      // max desired level (1-55)
-      maxLevel: 40,
-      // minimum stat:
-      //  0 - wild
-      //  1 - good friend trade
-      //  2 - great friend trade
-      //  3 - ultra friend trade
-      //  4 - weather boost
-      //  5 - best friend trade
-      // 10 - raid/egg/mission
-      // 12 - lucky
-      minimumStatValue: 0,
-    });
-    console.log({ rank, occurence });
+    // const pokedexEntry = getByName({ name: 'Ninetales' });
+    // const { rank, occurence } = calculateRank({
+    //   pokedexEntry,
+    //   // refs from pokémon being compared (1-15)
+    //   refAttackStat: 0,
+    //   refDefenseStat: 15,
+    //   refHealthStat: 15,
+    //   // max league CP
+    //   maxCP: 1500,
+    //   // max desired level (1-55)
+    //   maxLevel: 40,
+    //   // minimum stat:
+    //   //  0 - wild
+    //   //  1 - good friend trade
+    //   //  2 - great friend trade
+    //   //  3 - ultra friend trade
+    //   //  4 - weather boost
+    //   //  5 - best friend trade
+    //   // 10 - raid/egg/mission
+    //   // 12 - lucky
+    //   minimumStatValue: 0,
+    // });
+    // console.log({ rank, occurence });
   }
 }
--- src/app/pokemon-select/pokemon-select.component.ts
+++ src/app/pokemon-select/pokemon-select.component.ts
@@ -1,9 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
+import { PokemonInfo, PokemonListService } from './pokemon-list.service';
+import {
+  debounceTime,
+  filter,
+  map,
+  Observable,
+  startWith,
+} from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-select',
@@ -18,7 +26,20 @@ import { MatInputModule } from '@angular/material/input';
     ReactiveFormsModule,
   ],
 })
-export class PokemonSelectComponent {
+export class PokemonSelectComponent implements OnInit {
   formControl: FormControl = new FormControl();
-  filteredOptions: string[] = ['one', 'two', 'three'];
+  filteredOptions$!: Observable<PokemonInfo[]>;
+
+  constructor(private pokemonListService: PokemonListService) {}
+
+  ngOnInit(): void {
+    this.filteredOptions$ = this.formControl.valueChanges.pipe(
+      startWith(''),
+      filter(Boolean),
+      debounceTime(500),
+      map((searchTerm) => {
+        return this.pokemonListService.searchPokemonList(searchTerm);
+      })
+    );
+  }
 }
